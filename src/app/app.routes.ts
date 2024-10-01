fd25625061f88996febc8bcb9d37630380ec2aa6
unknown <mariamathiaw92@gmail.com>
2024-10-01 18:25:06 +0000

    intégrations du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent } // La page d'accueil, qui contient toutes les sections
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { 
+    scrollPositionRestoration: 'enabled',  // Restaure la position de défilement
+    anchorScrolling: 'enabled',  // Active le scrolling par ancres
+    scrollOffset: [0, 64]  // Décalage pour compenser la hauteur de la barre de navigation
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
