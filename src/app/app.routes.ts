a78b59d3171f22a31cc937d64e09323454e2d6a1
unknown <mariamathiaw92@gmail.com>
2024-10-08 10:15:12 +0000

    Intégration de la section du formulaire de modification d'annonce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { Dashboard1Component } from './components/Structure/dashboard1/dashboard
 import { AnnonceComponent } from './components/Structure/annonce/annonce.component';
 import { AjoutAnnonceComponent } from './components/Structure/ajout-annonce/ajout-annonce.component';
 import { DetailAnnonceComponent } from './components/Structure/detail-annonce/detail-annonce.component';
+import { ModifierAnnonceComponent } from './components/Structure/modifier-annonce/modifier-annonce.component';
 
 // Authentification
 export const routes: Routes = [
@@ -50,9 +51,9 @@ export const routes: Routes = [
     { path: 'dashboard1', component: Dashboard1Component },
     { path: 'annonce', component: AnnonceComponent },
     { path: 'annonce/ajouter', component: AjoutAnnonceComponent },
-    { path: 'annonce/detail-annonce', component: DetailAnnonceComponent }
-    /*{ path: 'annonce/modifier', component: DonneurStructureComponent }
-    { path: 'habitant', component: HabitantComponent },
+    { path: 'annonce/detail-annonce', component: DetailAnnonceComponent },
+    { path: 'annonce/modifier', component: ModifierAnnonceComponent }
+    /*{ path: 'habitant', component: HabitantComponent },
     { path: 'habitant/detail/habitant/:id', component: DetailHabitantComponent },
     { path: 'habitant/detail/projet/:id', component: DetailProjetHabitantComponent },
     {path: 'parametre', component: ParametreComponent},
