06903eca86a5152299b9d3043c5481f86861b56e
unknown <mariamathiaw92@gmail.com>
2024-11-02 15:51:20 +0000

    Affichage des donneurs externes sur la liste des donneurs .Aussi spécification du nombre de don des utilisateurs simples pour chaque structure

--- src/app/Services/donneur-externe.service.ts
+++ src/app/Services/donneur-externe.service.ts
@@ -28,6 +28,14 @@ export class DonneurExterneService {
     });
     return this.http.get<any[]>(`${apiUrl}/donneur-externes/${id}`, {headers});
   }
+  // Liste des donneurs pour chaque structure avec les infos sur le nombre et la date de don
+  getDonneursParStructure(){
+    const token = localStorage.getItem('access_token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get<any[]>(`${apiUrl}/structure/donneurs`, {headers});
+  }
 
 
   
--- src/app/components/Structure/donneur/donneur.component.ts
+++ src/app/components/Structure/donneur/donneur.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AnnonceService } from '../../../Services/annonce.service';
 import { CommonModule } from '@angular/common';
+import { DonneurExterneService } from '../../../Services/donneur-externe.service';
 
 @Component({
   selector: 'app-donneur',
@@ -13,16 +14,48 @@ import { CommonModule } from '@angular/common';
 export class DonneurComponent  implements OnInit {
   donneurs: any[] = [];
 
-  constructor(private annonceService: AnnonceService) { }
+  constructor(private annonceService: AnnonceService, private donneurExterneService: DonneurExterneService) { }
 
   ngOnInit() {
+     // Récupérer les utilisateurs simples
     this.annonceService.getUtilisateursWithCompletedInscriptions()
-      .subscribe((data: any) => {
-        this.donneurs = data;
-        console.log('Données récupérées :', this.donneurs); // Pour le débogage
-      }, error => {
-        console.error('Erreur lors de la récupération des utilisateurs :', error);
-      });
-  }
+    .subscribe((utilisateursSimples: any) => {
+      console.log('Utilisateurs simples récupérés :', utilisateursSimples);
 
+      // Récupérer les donneurs externes de la structure
+      this.donneurExterneService.getDonneursParStructure().subscribe(
+        (response: any) => {
+          if (response.status) {
+            // Ici, on considère que response.donneurs est un tableau d'objets
+            const donneursExternes = response.donneurs; // Type implicite
+
+            console.log('Donneurs externes récupérés :', donneursExternes);
+
+            // Mapper les donneurs externes pour correspondre aux attributs attendus
+            const donneursExternesMap = donneursExternes.map((donneur: any) => ({ // Utiliser 'any' pour le type
+              id: donneur.id,
+              nom_complet: `${donneur.prenom} ${donneur.nom}`, // Combiner nom et prénom
+              telephone: donneur.telephone,
+              groupe_sanguin: donneur.groupe_sanguin ,
+              nombre_de_dons: donneur.nombre_dons ,
+              dernier_don: donneur.dernier_don 
+            }));
+
+            // Fusionner les utilisateurs simples et les donneurs externes
+            this.donneurs = [...utilisateursSimples, ...donneursExternesMap];
+            console.log('Données combinées récupérées :', this.donneurs);
+          } else {
+            console.error("Erreur lors de la récupération des donneurs externes :", response.error);
+          }
+        },
+        (error) => {
+          console.error("Erreur lors de la récupération des donneurs externes :", error);
+        }
+      );
+    },
+    (error) => {
+      console.error("Erreur lors de la récupération des utilisateurs simples :", error);
+    }
+  );
+}
 }
