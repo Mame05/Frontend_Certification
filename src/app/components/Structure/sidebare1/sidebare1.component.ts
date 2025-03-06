5d3a42c5a2de01e984a379ed6625e5da5ed06840
Maréme Thiaw <mariamathiaw92@gmail.com>
2025-03-06 17:32:29 +0100

    Amélioratio de la responsive du sidebarre et du topbarre et de certaines pages

--- src/app/components/Structure/sidebare1/sidebare1.component.ts
+++ src/app/components/Structure/sidebare1/sidebare1.component.ts
@@ -82,8 +82,17 @@ deleteNotification(notificationId: number) {
 
 toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
+  const mainContent = document.querySelector('.main-content');
+  const topbar = document.querySelector('.topbar');
+
   if (sidebar) {
-      sidebar.classList.toggle('collapsed');
+    sidebar.classList.toggle('collapsed');
+  }
+  if (mainContent) {
+    mainContent.classList.toggle('expanded');
+  }
+  if (topbar) {
+    topbar.classList.toggle('expanded');
   }
 }
 }
