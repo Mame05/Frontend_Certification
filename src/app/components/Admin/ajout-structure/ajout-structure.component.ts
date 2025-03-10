35674edaa45af384ffbd430eeb328ea63f9aab5a
Maréme Thiaw <mariamathiaw92@gmail.com>
2025-03-10 14:06:36 +0100

    Responsivité de la totalité des pages du dasboard de l'admin

--- src/app/components/Admin/ajout-structure/ajout-structure.component.ts
+++ src/app/components/Admin/ajout-structure/ajout-structure.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {  ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { StructureService } from '../../../Services/structure.service';
+import Swal from 'sweetalert2'; // Importer SweetAlert
 
 
 @Component({
@@ -21,27 +22,56 @@ export class AjoutStructureComponent {
     private router: Router
   ) {
     this.structureForm = this.fb.group({
-      nom_structure: ['', Validators.required],
-      sigle: ['', Validators.required],
-      adresse: ['', Validators.required],
-      telephone: ['', Validators.required],
+      nom_structure: ['', [Validators.required,  Validators.pattern(/^[A-ZÀ-Ÿ][A-Za-zÀ-ÿ '-]*$/)]],
+      sigle: ['', [Validators.required, Validators.pattern(/^[A-Z0-9][A-Z0-9.-]*[A-Z0-9]$/)
+      ]],
+      adresse: ['', [Validators.required,  Validators.pattern(/^[A-ZÀ-Ÿ][A-Za-zÀ-ÿ0-9 ,.'\-+/]*$/)]],
+      telephone: ['', [Validators.required, Validators.pattern(/^(77|78|76|75|70)\s?\d{3}\s?\d{2}\s?\d{2}$/)]],
       region: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8),Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/)]],
     });
   }
 
   onSubmit() {
     if (this.structureForm.valid) {
       console.log(this.structureForm.value);  // Vérifier les données du formulaire
-      this.structureService.createStructure(this.structureForm.value).subscribe((response) => {
+      this.structureService.createStructure(this.structureForm.value).subscribe({
+        next: (response) => {
         console.log('Structure ajoutée avec succès', response);
-        this.router.navigate(['/sidebar/structure']);  // Rediriger vers la liste des structures
+        //Afficher l'alerte SweetAlert
+        Swal.fire({
+          icon: 'success',
+          title: '🎉 Structure ajoutée !',
+          text: 'La structure a été ajoutée avec succès.',
+          showConfirmButton: false,
+          timer: 2500// Ferme l'alerte après 3 secondes
+        }).then(() => {
+          // Redirection après fermeture de l'alerte
+          this.router.navigate(['/sidebar/structure']);
+        });
       },
-      
-    );
-    
+      error: (error) => {
+        console.error('Erreur lors de l\'ajout de la structure', error);
+        // Afficher une alerte en cas d'erreur
+        Swal.fire({
+          icon: 'error',
+          title: 'Échec de l\'ajout',
+          text: 'Une erreur est survenue, veuillez réessayer.',
+          confirmButtonText: 'OK'
+        });
+      },
+    });
+  } else {
+    //Afficher une alerte si le formulaire est invalide
+    Swal.fire({
+      icon: 'warning',
+      title: 'Champs invalides',
+      text: 'Veuillez remplir tous les champs correctement avant de soumettre.',
+      confirmButtonText: 'OK'
+    });
   }
+}
 
 }
-}
+
